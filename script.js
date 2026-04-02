e581fcf028b0490b48d5d05821c09447d0a7bc46
cortepiedra2026 <cortepiedra2024@gmail.com>
2026-04-02 16:59:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-window.onload = () => {
-setTimeout(() => {
-document.getElementById("loader").style.display = "none";
-}, 2000);
-};
+window.addEventListener("load", function(){
 
-function mostrarMenu(){
-document.getElementById("menu").style.display = "block";
-}
+setTimeout(function(){
 
-function mostrarCalificacion(){
-document.getElementById("calificacion").style.display = "block";
-}
+document.getElementById("loading").style.display="none";
+document.getElementById("main").style.display="block";
 
-function cerrar(){
-document.querySelectorAll(".panel").forEach(p => p.style.display = "none");
-}
+},2000)
 
-document.querySelectorAll(".stars").forEach(container => {
-for(let i=1;i<=5;i++){
-let star = document.createElement("span");
-star.innerHTML = "⭐";
-star.onclick = () => {
-container.setAttribute("data-value", i);
-}
-container.appendChild(star);
-}
-});
-
-function enviar(){
-emailjs.send("YOUR_SERVICE_ID","YOUR_TEMPLATE_ID",{
-nombre: document.getElementById("nombre").value,
-comida: document.querySelector('[data-type="comida"]').dataset.value,
-atencion: document.querySelector('[data-type="atencion"]').dataset.value,
-ambiente: document.querySelector('[data-type="ambiente"]').dataset.value,
-comentario: document.getElementById("comentario").value
-}).then(() => {
-alert("Calificación enviada 🔥");
-});
-}
+})
